539edc812dd675b6b75c512d6e3d5c54f7afc31b
Tom Kressman <tkressma@oswego.edu>
2022-01-10 18:23:25 -0500

    Add base project files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "./UI/Container";
+
+function App() {
+  return (
+    <Container>
+      <Header />
+      <Calculator />
+    </Container>
+  );
+}
+
+export default App;
